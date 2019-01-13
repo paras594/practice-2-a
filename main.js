639e2d18722988a356bb0298a876cedd6b3f1ccb
paras594 <parasarora594@gmail.com>
2019-01-13 10:39:52 +0530

    created and added content to html,css,js files

---
+++ main.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+   let body = document.querySelector("body");
+   let p = document.createElement("p");
+   p.textContent = "I am coming from javascript.";
+   body.appendChild(p);
+};
